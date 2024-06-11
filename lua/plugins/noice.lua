da55f8e51417a6da02c0222ac8546123f4ca75e8
Ting Zhou <zhoutingneyc@gmail.com>
2024-06-11 05:41:12 +0000

    fix(ui): adds nfv2 compat for noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,29 +4,48 @@ local safe_require = require("lib.meta").safe_require
 ---@diagnostic disable: missing-fields
 local noice_views = safe_require("plugins.noice_views").views
 
+local cmdline_format = {
+  -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+  -- view: (default is cmdline view)
+  -- opts: any options passed to the view
+  -- icon_hl_group: optional hl_group for the icon
+  -- title: set to anything or empty string to hide
+  cmdline = { pattern = "^:", icon = "", lang = "vim" },
+  search_down = { kind = "search", pattern = "^/", icon = "󰍉 ", lang = "regex" },
+  search_up = { kind = "search", pattern = "^%?", icon = "󰍉 ", lang = "regex" },
+  filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+  lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+  help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖" },
+  telescope = { pattern = "^:%s*Telescope%s+", icon = "", kind = "search" },
+  edit = { pattern = "^:%s*e%s+", icon = "󱇧" },
+  input = {}, -- Used by input()
+  -- lua = false, -- to disable a format, set to `false`
+}
+local nfv2_format = {
+  cmdline = { pattern = "^:", icon = "", lang = "vim" },
+  search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+  search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+  filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+  lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+  help = { pattern = "^:%s*he?l?p?%s+", icon = "?" },
+  telescope = { pattern = "^:%s*Telescope%s+", icon = "", kind = "search" },
+  edit = { pattern = "^:%s*e%s+", icon = "" },
+  input = {}, -- Used by input()
+}
+
+if os.getenv("NERDFONT") == "2" then
+  cmdline_format = nfv2_format
+end
+
+
+
 safe_require("noice").setup({
 	cmdline = {
 		enabled = true, -- enables the Noice cmdline UI
 		view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
 		opts = {}, -- global options for the cmdline. See section on views
 		---@type table<string, CmdlineFormat>
-		format = {
-			-- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
-			-- view: (default is cmdline view)
-			-- opts: any options passed to the view
-			-- icon_hl_group: optional hl_group for the icon
-			-- title: set to anything or empty string to hide
-			cmdline = { pattern = "^:", icon = "", lang = "vim" },
-			search_down = { kind = "search", pattern = "^/", icon = "󰍉 ", lang = "regex" },
-			search_up = { kind = "search", pattern = "^%?", icon = "󰍉 ", lang = "regex" },
-			filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-			lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-			help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖" },
-			telescope = { pattern = "^:%s*Telescope%s+", icon = "", kind = "search" },
-			edit = { pattern = "^:%s*e%s+", icon = "󱇧" },
-			input = {}, -- Used by input()
-			-- lua = false, -- to disable a format, set to `false`
-		},
+		format = cmdline_format,
 	},
 	messages = {
 		-- NOTE: If you enable messages, then the cmdline is enabled automatically.
