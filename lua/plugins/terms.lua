ccd3eae8bb49027b463a944013eaecfbd965de83
Ting Zhou <zhoutingneyc@gmail.com>
2024-06-11 05:22:38 +0000

    fix(ui): adds nfv2 compat for lazygit

--- lua/plugins/terms.lua
+++ lua/plugins/terms.lua
@@ -1,13 +1,17 @@
 local M = {}
 
 function M.lazy_git()
-	vim.cmd(
-		":FloatermNew --height=0.9 --width=0.9 --wintype=float --title=lazygit --name=LazyGit --autoclose=2 lazygit"
-	)
+  local ucf = ""
+  if os.getenv("NERDFONT") == "2" then
+    ucf = " -ucf=" .. os.getenv("XDG_CONFIG_HOME") .. "/lazygit/config_backup.yml"
+  end
+  vim.cmd(
+    ":FloatermNew --height=0.9 --width=0.9 --wintype=float --title=lazygit --name=LazyGit --autoclose=2 lazygit" .. ucf
+  )
 end
 
 function M.yazi()
-	vim.cmd(":FloatermNew yazi")
+  vim.cmd(":FloatermNew yazi")
 end
 
 return M
