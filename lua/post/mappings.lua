5661d22e40945c76bd72c6514d8f075bafab54d2
Ting Zhou <zhoutingneyc@gmail.com>
2024-07-23 20:35:33 +0800

    chore: tab-switching maps and snippet updates

--- lua/post/mappings.lua
+++ lua/post/mappings.lua
@@ -37,6 +37,18 @@ local key_configs = {
     ["[c"] = key_utils.prev_hunk,
     ["]c"] = key_utils.next_hunk,
 
+    -- buffer movement
+    ["<leader>1"] = "<Cmd>BufferLineGoToBuffer 1<CR>",
+    ["<leader>2"] = "<Cmd>BufferLineGoToBuffer 2<CR>",
+    ["<leader>3"] = "<Cmd>BufferLineGoToBuffer 3<CR>",
+    ["<leader>4"] = "<Cmd>BufferLineGoToBuffer 4<CR>",
+    ["<leader>5"] = "<Cmd>BufferLineGoToBuffer 5<CR>",
+    ["<leader>6"] = "<Cmd>BufferLineGoToBuffer 6<CR>",
+    ["<leader>7"] = "<Cmd>BufferLineGoToBuffer 7<CR>",
+    ["<leader>8"] = "<Cmd>BufferLineGoToBuffer 8<CR>",
+    ["<leader>9"] = "<Cmd>BufferLineGoToBuffer 9<CR>",
+    ["<leader>$"] = "<Cmd>BufferLineGoToBuffer -1<CR>",
+
     -- Telescope
 
     -- local files
