8702c25fc2764b47dad13e6b00a85177675dfb5c
Ting Zhou <zhoutingneyc@gmail.com>
2024-08-10 18:11:44 +0800

    feat: adds obsidian.nvim

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    lazy = true,
+    ft = "markdown",
+    opts = {
+      workspaces = {
+        {
+          name = "Vault",
+          path = "~/Documents/Vault"
+        },
+      },
+    },
+  }
+}
