eeef267a672c467e0a34730bd99bdfe3983f64b3
Ting Zhou <zhoutingneyc@gmail.com>
2024-08-30 17:41:27 -0400

    feat: adopts openrouter for apikey and endpoint
    claude sonnet would have been nice, but it is much more expensive and
    has some weird behaviors when streaming results. deepseek is the second
    best option.

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -54,20 +54,48 @@ return {
   {
     "yetone/avante.nvim",
     cmd = "AvanteAsk",
-    opts = {
-      provider = "openai",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-3.5-turbo",
-        timeout = 30000, -- Timeout in milliseconds
-        temperature = 0,
-        max_tokens = 4096,
-        ["local"] = false,
-      },
-    },
+    config = function()
+      require("avante").setup({
+        provider = "openrouter",
+        vendors = {
+          openrouter = {
+            api_key_name = "OPENROUTER_API_KEY",
+            endpoint = "https://openrouter.ai/api/v1/chat/completions",
+            -- model = "anthropic/claude-3.5-sonnet",
+            model = "deepseek/deepseek-coder",
+            parse_curl_args = function(opts, code_opts)
+              return {
+                url = opts.endpoint,
+                headers = {
+                  ["Content-Type"] = "application/json",
+                  ["Authorization"] = "Bearer " .. os.getenv(opts.api_key_name),
+                },
+                body = {
+                  model = opts.model,
+                  messages = require("avante.providers").openai.parse_message(code_opts), -- you can make your own message, but this is very advanced
+                  temperature = 0,
+                  max_tokens = 4096,
+                  stream = true,
+                },
+              }
+            end,
+            parse_response_data = function(data_stream, event_state, opts)
+              require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+            end,
+          },
+        },
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-3.5-turbo",
+          timeout = 30000, -- Timeout in milliseconds
+          temperature = 0,
+          max_tokens = 4096,
+          ["local"] = false,
+        },
+      })
+    end,
     keys = {
       { "<leader>aa", function() require("avante.api").ask() end,     desc = "avante: ask",    mode = { "n", "v" } },
-      { "<leader>ar", function() require("avante.api").refresh() end, desc = "avante: refresh" },
       { "<leader>ae", function() require("avante.api").edit() end,    desc = "avante: edit",   mode = "v" },
     },
     dependencies = {
