640dcca2c48e7435ff2bb4e126539b0f7394eb72
Ting Zhou <zhoutingneyc@gmail.com>
2024-08-30 12:41:33 -0400

    feat: modern markdown editing mode

--- lua/plugins/langs.lua
+++ lua/plugins/langs.lua
@@ -1,6 +1,31 @@
 return {
   -- languages
-  { "preservim/vim-markdown", branch = "master", ft = "markdown" },
+  {
+    "OXY2DEV/markview.nvim",
+    ft = "markdown", -- If you decide to lazy-load anyway
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+
+      "nvim-tree/nvim-web-devicons"
+    },
+    opts = {
+      modes = { "n", "no", "c" }, -- Change these modes
+      -- to what you need
+
+      hybrid_modes = { "n" }, -- Uses this feature on
+      -- normal mode
+      -- This is nice to have
+      callbacks = {
+        on_enable = function(_, win)
+          vim.wo[win].conceallevel = 2;
+          vim.wo[win].concealcursor = "c";
+        end
+      }
+    }
+  },
   -- for advanced usage
-  { "LhKipp/nvim-nu",         ft = "nu",         opts = { use_lsp_features = false } },
+  { "LhKipp/nvim-nu", ft = "nu", opts = { use_lsp_features = false } },
 }
