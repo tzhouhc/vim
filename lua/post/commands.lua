15e62450327cc6a8237651193d547c360ed07338
Ting Zhou <zhoutingneyc@gmail.com>
2025-04-21 15:19:54 -0400

    feat: more telescope commands

--- lua/post/commands.lua
+++ lua/post/commands.lua
@@ -5,6 +5,7 @@ local popups = require("lib.popups")
 local tools = require("lib.tools")
 local lazygit = require("snacks.lazygit")
 local rename = require("snacks.rename")
+local ts = require("telescope.builtin")
 
 vim.api.nvim_create_user_command("SelectSession", function()
 	require("persistence").select()
@@ -25,10 +26,10 @@ vim.api.nvim_create_user_command("FormatCode", tools.smart_format, { range = tru
 vim.api.nvim_create_user_command("Git", function() lazygit.open() end, {})
 vim.api.nvim_create_user_command("GitLinesLogs", terms.git_lines_log, { range = true })
 vim.api.nvim_create_user_command("GitLinesBlame", terms.git_lines_blame, { range = true })
+
 -- Others
 vim.api.nvim_create_user_command("FloatRight", terms.right_side_term, {})
 vim.api.nvim_create_user_command("H", popups.help_popup, { nargs = "?", complete = "help" })
-
 vim.api.nvim_create_user_command("Scratch", misc.make_scratch, {})
 
 -- Snacks
@@ -36,3 +37,11 @@ vim.api.nvim_create_user_command("RenameFile", rename.rename_file, {})
 
 -- Diagnostics
 vim.api.nvim_create_user_command("Diagnostics", "Trouble diagnostics win.type=split win.position=right win.size.width=80", {})
+
+-- Telescopes
+vim.api.nvim_create_user_command("Symbols", ts.lsp_document_symbols, {})
+vim.api.nvim_create_user_command("WorkspaceSymbols", ts.lsp_workspace_symbols, {})
+vim.api.nvim_create_user_command("Registers", ts.registers, {})
+vim.api.nvim_create_user_command("Highlights", ts.highlights, {})
+vim.api.nvim_create_user_command("Manpages", ts.man_pages, {})
+
