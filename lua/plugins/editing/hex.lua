c9706c0de31b8368c3dc54acf216f278aeb2419c
Ting Zhou <zhoutingneyc@gmail.com>
2025-04-25 21:30:48 +0000

    feat: add hex-editing tool

---
+++ lua/plugins/editing/hex.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'RaafatTurki/hex.nvim',
+    config = function()
+      local hex = require('hex')
+      hex.setup()
+      vim.api.nvim_create_user_command("Hex", hex.toggle, {})
+    end,
+    cmd = { 'Hex' },
+  }
+}
