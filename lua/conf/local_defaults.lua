c0ba24ceaa877ef967d9ab3df4b8a338f353642e
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-11 23:28:55 -0400

    cleanup: unused confgs

--- lua/conf/local_defaults.lua
+++
@@ -1,8 +0,0 @@
--- vim overall colorscheme, options are:
---   "nord", "catppuccin"
-vim.g.theme = "nord"
-
-vim.g.smear_cursor = false
-vim.g.animate_scroll = false
-vim.g.enable_noice = true
-vim.g.enable_ufo = true
