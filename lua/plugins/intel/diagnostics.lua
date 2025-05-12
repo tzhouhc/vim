abe752645d88bb96ec343c03aaa67f6d5ccbd305
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-11 21:21:46 -0400

    fix: diagnostics not showing

--- lua/plugins/intel/diagnostics.lua
+++ lua/plugins/intel/diagnostics.lua
@@ -1,7 +1,6 @@
 return {
   {
     "rachartier/tiny-inline-diagnostic.nvim",
-    event = "LspAttach",
     priority = 1000, -- needs to be loaded in first
     config = function()
       require('tiny-inline-diagnostic').setup()
