cb2dfdc84a51c524c7c63ba7af829f0ff4d53416
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-11 20:50:24 -0400

    cleanup: remove custom mappings

--- lua/conf/mappings.lua
+++ lua/conf/mappings.lua
@@ -6,77 +6,12 @@
 local key_configs = {
   -- Normal mode
   n = {
-    -- cancel search/flash highlight
-    ["<esc>"] = "<esc>:noh<CR>",
-    -- close all non-main windows
-    ["<leader><esc>"] = ":only<CR>",
-    -- close this window
-    ["<m-esc>"] = ":q<CR>",
-    -- leader actions
-    ["<leader>ev"] = ":e $MYVIMRC<cr>",
-
-    -- disable cmdline window
-    ["q:"] = "",
-
-    -- buffer movement
-    ["[b"] = ":bprev<cr>",
-    ["]b"] = ":bnext<cr>",
-    ["<PageUp>"] = ":bprev<cr>",
-    ["<PageDown>"] = ":bnext<cr>",
-    ["[t"] = ":tabp<cr>",
-    ["]t"] = ":tabn<cr>",
-
-    -- tmux-like splitting
-    ["<c-w>%"] = ":vsplit<cr>",
-    ['<c-w>"'] = ":split<cr>",
-    ["<c-w>z"] = ":only<cr>",
-    -- alternate window movement
-    ["[w"] = "<c-w><left>",
-    ["]w"] = "<c-w><right>",
-    -- quicklist movement
-    ["[q"] = ":cprev<cr>",
-    ["]q"] = ":cnext<cr>",
-    ["[Q"] = ":cfirst<cr>",
-    ["]Q"] = ":clast<cr>",
-
-    -- increment/decrement number
-    ["+"] = "<c-a>",
-    ["-"] = "<c-x>",
   },
   -- Visual mode
   v = {
-    -- select whole words by default
-    ["w"] = "iw",
-    ["W"] = "iW",
-    [")"] = "i)",
-    ["]"] = "i]",
-    ['"'] = 'i"',
-
-    -- replace currently selected text with default register
-    -- without yanking it.
-    ["p"] = '"_dP',
   },
   [{ "n", "v" }] = {
-    -- faster movement
-    ["<c-Up>"] = "10k",
-    ["<c-Down>"] = "10j",
-    -- better cutting (`x` no longer yanks due to cutlass,
-    -- but it doesn't delete the whole line.)
-    ["X"] = '"_dd',
   },
-  [{ "i", "s" }] = {
-    -- enter new line but don't keep commenting if currently in comment block
-    ["<s-cr>"] = "<esc>o<esc>cc",
-  },
-  -- Terminal mode
-  t = {
-    -- terminal mode exit
-    ["<c-w><esc>"] = "<C-\\><C-n>",
-  },
-  ca = {
-    ["bd"] = "BD!",
-    ["waq"] = "wa | q",
-  }
 }
 
 require("lib.misc").batch_set_keymap(key_configs)
--- lua/post/mappings.lua
+++ lua/post/mappings.lua
@@ -1,7 +1,6 @@
 -- Keyboard Mapping Configurations, with Plugins
 
 local key_utils = require("lib.key_utils")
-local terms = require("lib.terms")
 
 -- For automating setting key maps.
 -- Usage: highest level keys are modes;
@@ -9,79 +8,14 @@ local terms = require("lib.terms")
 local key_configs = {
   -- Normal mode
   n = {
-    ["<leader>q"] = ":BD<cr>",
-    ["<leader>ql"] = key_utils.toggle_quickfix,
-    ["<leader>lg"] = ":Git<cr>",
-    ["<leader>nm"] = ":Namu symbols<cr>",
-
-    -- diffview
-    ["<leader>dv"] = key_utils.toggle_diffview,
-
-    -- quake term
-    ["<m-e>"] = terms.quake_term,
-
-    -- diagnostics movement
-    ["[e"] = function() vim.diagnostic.jump({ count = -1 }) end,
-    ["]e"] = function() vim.diagnostic.jump({ count = 1 }) end,
-    -- create empty lines without moving
-    ["[<space>"] = key_utils.add_blank_line_before,
-    ["]<space>"] = key_utils.add_blank_line_after,
-    -- move between change hunks
-    ["[c"] = key_utils.prev_hunk,
-    ["]c"] = key_utils.next_hunk,
-
-    -- buffer movement
-    ["<c-1>"] = "<Cmd>BufferLineGoToBuffer 1<CR>",
-    ["<c-2>"] = "<Cmd>BufferLineGoToBuffer 2<CR>",
-    ["<c-3>"] = "<Cmd>BufferLineGoToBuffer 3<CR>",
-    ["<c-4>"] = "<Cmd>BufferLineGoToBuffer 4<CR>",
-    ["<c-5>"] = "<Cmd>BufferLineGoToBuffer 5<CR>",
-    ["<c-6>"] = "<Cmd>BufferLineGoToBuffer 6<CR>",
-    ["<c-7>"] = "<Cmd>BufferLineGoToBuffer 7<CR>",
-    ["<c-8>"] = "<Cmd>BufferLineGoToBuffer 8<CR>",
-    ["<c-9>"] = "<Cmd>BufferLineGoToBuffer 9<CR>",
-    ["<c-0>"] = "<Cmd>BufferLineGoToBuffer -1<CR>",
-
-    -- formatting
-    ["<leader>fc"] = "<Cmd>FormatCode<cr>",
-
-    -- local fuzzy find
-    ["<m-f>"] = terms.repo_live_grep,
-
-    -- smarter shift I
-    ["I"] = key_utils.smarter_shift_i,
-    ["<m-i>"] = key_utils.smart_move_to_start_and_insert,
   },
   -- Visual mode
   v = {
-    -- formatting
-    ["<leader>fc"] = "<Cmd>FormatCode<cr>",
   },
   [{ "n", "v" }] = {
-    -- jump to first position after the first space (to avoid comment prefixes).
-    ["0"] = key_utils.alternating_zero,
-    -- jump to start of text object, be situationally aware
-    ["<m-0>"] = key_utils.smart_move_to_start,
-    ["<m-left>"] = key_utils.smart_move_to_start,
-    ["<m-right>"] = key_utils.smart_move_to_end,
   },
   [{ "i", "s" }] = {
-    -- fallback value of these commands are hardcoded to tab and s-tab, so
-    -- change the corresponding func if changed here.
-    ["<c-right>"] = { key_utils.vsnip_jump_forward, { expr = true } },
-    ["<c-left>"] = { key_utils.vsnip_jump_backward, { expr = true } },
   },
-  -- Terminal mode
-  t = {
-  },
-  -- Command mode, abbrev
-  ca = {
-    ["h"] = "H",
-  },
-  -- selection by textobject
-  [{ "o", "x" }] = {
-    ["ih"] = ":<C-U>Gitsigns select_hunk<CR>",
-  }
 }
 
 require("lib.misc").batch_set_keymap(key_configs)
