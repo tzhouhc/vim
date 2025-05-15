6894500a68669ef86fb6def20413048e03dd92f7
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-15 10:25:17 -0400

    fix: update nvim api call

--- lua/lib/windows.lua
+++ lua/lib/windows.lua
@@ -8,7 +8,7 @@ function M.is_win_open_with_type(t)
     -- Get the buffer ID associated with the window
     local buf = vim.api.nvim_win_get_buf(win)
     -- Get the filetype of the buffer
-    local filetype = vim.api.nvim_buf_get_option(buf, 'filetype')
+    local filetype = vim.api.nvim_get_option_value('filetype', { buf = buf })
     -- Check if the filetype is 'help'
     if filetype == t then
       return true
