7e2efb1ca3600ab57e58b7489b71a689fef3d075
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-16 07:32:32 -0400

    feat: completion lsp kind text in addition to icon

--- lua/post/completions.lua
+++ lua/post/completions.lua
@@ -18,6 +18,35 @@ local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
+local format_cmp = function(entry, vim_item)
+  local kind = require("lspkind").cmp_format({
+    mode = "symbol_text",
+  })(entry, vim.deepcopy(vim_item))
+  local kind_map = {
+    Variable = "Var ",
+    Function = "Func",
+    Snippet = "Snip",
+    Field = "Fld ",
+    Property = "Prty",
+    Keyword = "Kwrd"
+  }
+  local kind_text = kind_map[vim_item.kind] or vim_item.kind
+  local highlights_info = require("colorful-menu").cmp_highlights(entry)
+
+  -- highlight_info is nil means we are missing the ts parser, it's
+  -- better to fallback to use default `vim_item.abbr`. What this plugin
+  -- offers is two fields: `vim_item.abbr_hl_group` and `vim_item.abbr`.
+  if highlights_info ~= nil then
+    vim_item.abbr_hl_group = highlights_info.highlights
+    vim_item.abbr = highlights_info.text
+  end
+  local strings = vim.split(kind.kind, "%s", { trimempty = true })
+  vim_item.kind = kind_text .. " " .. (strings[1] or "") .. " "
+  vim_item.menu = ""
+
+  return vim_item
+end
+
 cmp.setup({
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
@@ -42,26 +71,7 @@ cmp.setup({
   },
   formatting = {
     fields = { "kind", "abbr", "menu" },
-
-    format = function(entry, vim_item)
-      local kind = require("lspkind").cmp_format({
-        mode = "symbol_text",
-      })(entry, vim.deepcopy(vim_item))
-      local highlights_info = require("colorful-menu").cmp_highlights(entry)
-
-      -- highlight_info is nil means we are missing the ts parser, it's
-      -- better to fallback to use default `vim_item.abbr`. What this plugin
-      -- offers is two fields: `vim_item.abbr_hl_group` and `vim_item.abbr`.
-      if highlights_info ~= nil then
-        vim_item.abbr_hl_group = highlights_info.highlights
-        vim_item.abbr = highlights_info.text
-      end
-      local strings = vim.split(kind.kind, "%s", { trimempty = true })
-      vim_item.kind = " " .. (strings[1] or "") .. " "
-      vim_item.menu = ""
-
-      return vim_item
-    end,
+    format = format_cmp,
   },
   mapping = {
     ["<Tab>"] = cmp.mapping(function(fallback)
