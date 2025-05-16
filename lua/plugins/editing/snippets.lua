64b45bf2dc278248eaeb4a6374eecfe3ce4def6f
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-16 11:18:29 -0400

    feat: scissors for creating/editing vsc snippets

--- lua/plugins/editing/snippets.lua
+++ lua/plugins/editing/snippets.lua
@@ -1,3 +1,11 @@
 return {
   "rafamadriz/friendly-snippets",
+  {
+    "chrisgrieser/nvim-scissors",
+    cmd = { "ScissorsAddNewSnippet", "ScissorsEditSnippet" },
+    dependencies = "nvim-telescope/telescope.nvim", -- if using telescope
+    opts = {
+      snippetDir = os.getenv("VIM_HOME") .. "/snippets",
+    },
+  },
 }
