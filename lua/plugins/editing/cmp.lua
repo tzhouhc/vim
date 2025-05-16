fc22daab56200f98c613d4c0fd742e431e6b3aba
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-16 07:08:29 -0400

    cleanup: slight dry

--- lua/plugins/editing/cmp.lua
+++ lua/plugins/editing/cmp.lua
@@ -1,11 +1,13 @@
+local events = { "BufReadPost", "BufNewFile", "BufWritePre" }
+
 return {
   -- completions
-  { "hrsh7th/nvim-cmp",          event = { "BufReadPost", "BufNewFile", "BufWritePre" } },
-  { "hrsh7th/cmp-cmdline",       event = { "BufReadPost", "BufNewFile", "BufWritePre" } },
-  { "hrsh7th/cmp-buffer",        event = { "BufReadPost", "BufNewFile", "BufWritePre" } },
-  { "hrsh7th/cmp-calc",          event = { "BufReadPost", "BufNewFile", "BufWritePre" } },
-  { "hrsh7th/cmp-nvim-lsp",      event = { "BufReadPost", "BufNewFile", "BufWritePre" } },
-  { "hrsh7th/cmp-path",          event = { "BufReadPost", "BufNewFile", "BufWritePre" } },
-  { "PhilippFeO/cmp-help-tags",  event = { "BufReadPost", "BufNewFile", "BufWritePre" } },
-  { "xzbdmw/colorful-menu.nvim", event = { "BufReadPost", "BufNewFile", "BufWritePre" } },
+  { "hrsh7th/nvim-cmp",          event = events },
+  { "hrsh7th/cmp-cmdline",       event = events },
+  { "hrsh7th/cmp-buffer",        event = events },
+  { "hrsh7th/cmp-calc",          event = events },
+  { "hrsh7th/cmp-nvim-lsp",      event = events },
+  { "hrsh7th/cmp-path",          event = events },
+  { "PhilippFeO/cmp-help-tags",  event = events },
+  { "xzbdmw/colorful-menu.nvim", event = events },
 }
