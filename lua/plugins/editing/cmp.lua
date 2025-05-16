54fe77f0c2abb71187fa572d1f8e555813f71fb2
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-16 09:21:30 -0400

    partial: migrate towards blink-cmp

--- lua/plugins/editing/cmp.lua
+++ lua/plugins/editing/cmp.lua
@@ -2,12 +2,33 @@ local events = { "BufReadPost", "BufNewFile", "BufWritePre" }
 
 return {
   -- completions
-  { "hrsh7th/nvim-cmp",          event = events },
-  { "hrsh7th/cmp-cmdline",       event = events },
-  { "hrsh7th/cmp-buffer",        event = events },
-  { "hrsh7th/cmp-calc",          event = events },
-  { "hrsh7th/cmp-nvim-lsp",      event = events },
-  { "hrsh7th/cmp-path",          event = events },
-  { "PhilippFeO/cmp-help-tags",  event = events },
-  { "xzbdmw/colorful-menu.nvim", event = events },
+  {
+    "saghen/blink.cmp",
+    event = events,
+    config = true,
+    opts = {
+      keymap = {
+        ['<Tab>'] = {
+          'select_next',
+          'snippet_forward',
+          'fallback'
+        },
+        ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
+        ['<CR>'] = { 'accept', 'fallback' },
+      }
+    }
+  },
+  {
+    "saghen/blink.compat",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+    event = events,
+    opts = {
+      sources = {
+        -- remember to enable your providers here
+        default = { 'lsp', 'vsnip', 'buffer', 'path' },
+      },
+    }
+  },
 }
--- lua/plugins/editing/snippets.lua
+++ lua/plugins/editing/snippets.lua
@@ -1,5 +1,3 @@
 return {
-  "hrsh7th/cmp-vsnip",
-  "hrsh7th/vim-vsnip",
   "rafamadriz/friendly-snippets",
 }
