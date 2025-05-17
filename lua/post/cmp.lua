5b6e78c7f73451440dc62850626d67d872bf6a19
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-17 02:18:45 -0400

    fix: help-command completion

--- lua/post/cmp.lua
+++ lua/post/cmp.lua
@@ -23,3 +23,13 @@ vim.api.nvim_set_hl(0, "BlinkCmpKindText",
   { bg = bg, fg = colors.fg_by_hlgroup("BlinkCmpLabel") })
 vim.api.nvim_set_hl(0, "BlinkCmpKindSnippet",
   { bg = bg, fg = colors.fg_by_hlgroup("BlinkCmpLabel") })
+
+-- customize help command prefix check -- we use 'H' instead of 'h'
+local consts = require("blink.cmp.sources.cmdline.constants")
+consts.help_commands = {
+  help = true,
+  hel = true,
+  he = true,
+  h = true,  -- these are not actually needed since h gets turned into H anyway
+  H = true,
+}
