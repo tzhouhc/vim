06e29cc8414da4b196ba2c48891f33ccb550a7bc
Ting Zhou <zhoutingneyc@gmail.com>
2025-05-27 07:02:49 -0400

    fix: deprecated command

--- lua/plugins/passive/snacks.lua
+++ lua/plugins/passive/snacks.lua
@@ -29,15 +29,14 @@ local dash_header =
     "        ██▓▓░░  ▓▓██░░  ░░██▓▓  ░░▓▓██            \n"
 
 local dash_keys = {
-  dashboard_key_conf("0", "", "Recent Files", ":FzfLua oldfiles"),
   dashboard_key_conf("1", "", "New File", ":ene"),
   dashboard_key_conf("2", "󰍉", "Find File", ":FzfLua files"),
   dashboard_key_conf("3", "", "Last File", ":e#<1"),
   dashboard_key_conf("4", "󰏗", "Last Session", ":SessionLoadLast"),
   dashboard_key_conf("5", "󰏗", "Local Session", ":SessionLoadHere"),
+  dashboard_key_conf("r", "", "Recent Files", ":FzfLua oldfiles"),
   dashboard_key_conf("y", "󰇥", "File Explorer", ":Yazi"),
   dashboard_key_conf("s", "󰏗", "Select Session", ":SelectSession"),
-  dashboard_key_conf("g", "󰊢", "Repositories", ":SelectFromRepositories"),
   dashboard_key_conf("z", "󰚥", "Lazy", ":Lazy"),
   dashboard_key_conf("q", "󰠚", "Quit NVIM", ":qa"),
 }
