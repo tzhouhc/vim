111cab3c72e78168534b159604138773e9da9465
Ting Zhou <zhoutingneyc@gmail.com>
2025-06-18 03:32:50 -0400

    feat(ai): add more flexible ai plugin

--- lua/plugins/tools/ai.lua
+++ lua/plugins/tools/ai.lua
@@ -29,4 +29,41 @@ return {
       "nvim-telescope/telescope.nvim"
     }
   },
+  {
+    "olimorris/codecompanion.nvim",
+    cmd = {
+      "CodeCompanion",
+      "CodeCompanionActions",
+      "CodeCompanionChat",
+      "CodeCompanionCmd",
+    },
+    keys = {
+      { "cc", mode = "ca" },
+    },
+    config = function()
+      require("codecompanion").setup({
+        display = {
+          chat = {
+            window = {
+              layout = "float",
+              position = "right",
+            }
+          },
+        },
+        strategies = {
+          chat = {
+            adapter = "openai",
+          },
+          inline = {
+            adapter = "openai",
+          },
+        },
+      })
+      vim.keymap.set("ca", "cc", "CodeCompanion", { noremap = true, silent = true })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
 }
