ba7278d2b010ddcbbe2e6577e44eca6856f12b3a
Ting Zhou <zhoutingneyc@gmail.com>
2025-06-19 19:19:32 -0400

    fix(devicon): lazy version config

--- lua/plugins/ui/icons.lua
+++ lua/plugins/ui/icons.lua
@@ -1,11 +1,13 @@
-return {
-  -- icons
-  {
-    "nvim-tree/nvim-web-devicons",
-    tag = function()
-      if vim.g.nerdfont_v2_compat then
-        return "nerd-v2-compat"
-      end
-    end,
+if vim.g.nerdfont_v2_compat then
+  return {
+    -- icons
+    {
+      "nvim-tree/nvim-web-devicons",
+      tag = "nerd-v2-compat",
+    }
   }
-}
+else
+  return {
+    "nvim-tree/nvim-web-devicons",
+  }
+end
