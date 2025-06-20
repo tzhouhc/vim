211d5534e41a040404a0c86c852fc23d77bf33ba
Ting Zhou <zhoutingneyc@gmail.com>
2025-06-20 03:51:00 -0400

    feat(highlight): more efficient bindings for hilighting words

--- lua/plugins/visual/highlights.lua
+++ lua/plugins/visual/highlights.lua
@@ -9,17 +9,33 @@ return {
   -- highlight words under cursor
   {
     "Mr-LLLLL/interestingwords.nvim",
-    keys = { { "<leader>k", "n" }, { "<leader>m", "n" } },
-    opts = {
-      colors = { '#8fbcbb', '#88c0d0', '#bf616a', '#d08770', '#ebcb8b', '#a3be8c' },
-      search_count = true,
-      navigation = true,
-      scroll_center = false,
-      search_key = "<leader>m",
-      cancel_search_key = "<leader>M",
-      color_key = "<leader>k",
-      cancel_color_key = "<leader>K",
-      select_mode = "random", -- random or loop
-    }
+    cmd = { "HighlightWord" },
+    keys = { "?" },
+    config = function()
+      local iw = require("interestingwords")
+      iw.setup({
+        colors = { '#8fbcbb', '#88c0d0', '#bf616a', '#d08770', '#ebcb8b', '#a3be8c' },
+        search_count = true,
+        navigation = true,
+        scroll_center = false,
+        search_key = nil,
+        cancel_search_key = nil,
+        color_key = "<leader>k",
+        cancel_color_key = "<leader><esc>",
+        select_mode = "random", -- random or loop
+      })
+
+      -- I don't really ever search backwards, so we use `?` as
+      -- "higlight + search", and it also does the normal + visual combined
+      -- thing.
+      vim.keymap.set('n', "?", function()
+        iw.InterestingWord('n', false)
+        iw.InterestingWord('n', true)
+      end, { noremap = true, silent = true, desc = "InterestingWord Toggle Search" })
+      vim.keymap.set('x', "?", function()
+        iw.InterestingWord('v', false)
+        iw.InterestingWord('v', true)
+      end, { noremap = true, silent = true, desc = "InterestingWord Toggle Search" })
+    end,
   },
 }
