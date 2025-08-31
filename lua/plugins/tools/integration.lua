c6baf749bbbfdcecc99eaadfd914fd42c05b145b
Ting Zhou <zhoutingneyc@gmail.com>
2025-08-31 21:39:47 +0000

    fix: obsidian plugin error when vault not present

--- lua/plugins/tools/integration.lua
+++ lua/plugins/tools/integration.lua
@@ -3,6 +3,7 @@ return {
     "obsidian-nvim/obsidian.nvim",
     lazy = true,
     ft = "markdown",
+    cond = os.execute("test -d " .. os.getenv("HOME") .. "/Documents/Vault") == 0,
     opts = {
       workspaces = {
         {
