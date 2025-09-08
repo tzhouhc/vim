28e37095ec319596c671302c581d3ea51756f90c
Ting Zhou <zhoutingneyc@gmail.com>
2025-09-08 12:38:55 -0400

    feat: add gitlinker

--- lua/plugins/tools/integration.lua
+++ lua/plugins/tools/integration.lua
@@ -12,5 +12,22 @@ return {
         },
       },
     },
+  },
+  {
+    "ruifm/gitlinker.nvim",
+    config = function()
+      local gl = require("gitlinker")
+      local copy_url_and_open = function(data)
+        gl.actions.copy_to_clipboard(data)
+        gl.actions.open_in_browser(data)
+      end
+      gl.setup()
+      vim.api.nvim_create_user_command("GetGithubLinkToLine", function()
+        gl.get_buf_range_url("n", {action_callback = copy_url_and_open})
+      end, {})
+      vim.api.nvim_create_user_command("GetGithubLink", function()
+        gl.get_repo_url({action_callback = copy_url_and_open})
+      end, {})
+    end,
   }
 }
