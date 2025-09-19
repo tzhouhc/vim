a45577838503c74e822a1fd980888d2e2c13aa6b
Ting Zhou <zhoutingneyc@gmail.com>
2025-09-19 11:50:23 -0400

    cleanup: remove obsidian plugin

--- lua/plugins/tools/integration.lua
+++ lua/plugins/tools/integration.lua
@@ -1,19 +1,5 @@
 return {
   {
-    "obsidian-nvim/obsidian.nvim",
-    lazy = true,
-    ft = "markdown",
-    cond = os.execute("test -d " .. os.getenv("HOME") .. "/Documents/Vault") == 0,
-    opts = {
-      workspaces = {
-        {
-          name = "personal",
-          path = "~/Documents/Vault",
-        },
-      },
-    },
-  },
-  {
     "ruifm/gitlinker.nvim",
     config = function()
       local gl = require("gitlinker")
