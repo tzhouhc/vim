8836aee0a594fbc474129b3d9fd1ca54abe77d5e
Ting Zhou <zhoutingneyc@gmail.com>
2025-12-10 21:05:58 -0500

    feat: task picker

--- lua/plugins/tools/integration.lua
+++ lua/plugins/tools/integration.lua
@@ -20,62 +20,76 @@ return {
   {
     "bngarren/checkmate.nvim",
     ft = "markdown",
-    opts = {
-      files = { "*tasks.md" },
-      keys = {
-        ["gt"] = {
-          rhs = "<cmd>Checkmate toggle<CR>",
-          desc = "Toggle todo item",
-          modes = { "n", "v" },
+    config = function()
+      opts = {
+        files = { "*tasks.md" },
+        keys = {
+          ["gt"] = {
+            rhs = "<cmd>Checkmate toggle<CR>",
+            desc = "Toggle todo item",
+            modes = { "n", "v" },
+          },
+          ["g]"] = {
+            rhs = "<cmd>Checkmate cycle_next<CR>",
+            desc = "Cycle todo item(s) to the next state",
+            modes = { "n", "v" },
+          },
+          ["g["] = {
+            rhs = "<cmd>Checkmate cycle_previous<CR>",
+            desc = "Cycle todo item(s) to the previous state",
+            modes = { "n", "v" },
+          },
         },
-        ["g]"] = {
-          rhs = "<cmd>Checkmate cycle_next<CR>",
-          desc = "Cycle todo item(s) to the next state",
-          modes = { "n", "v" },
-        },
-        ["g["] = {
-          rhs = "<cmd>Checkmate cycle_previous<CR>",
-          desc = "Cycle todo item(s) to the previous state",
-          modes = { "n", "v" },
-        },
-      },
-      todo_states = {
-        -- Built-in states (cannot change markdown or type)
-        unchecked = { marker = "󰄱" },
-        checked = { marker = "" },
+        todo_states = {
+          -- Built-in states (cannot change markdown or type)
+          unchecked = { marker = "󰄱" },
+          checked = { marker = "" },
 
-        -- Custom states
-        in_progress = {
-          marker = "",
-          markdown = ".", -- Saved as `- [.]`
-          type = "incomplete", -- Counts as "not done"
-          order = 50,
+          -- Custom states
+          in_progress = {
+            marker = "",
+            markdown = ".", -- Saved as `- [.]`
+            type = "incomplete", -- Counts as "not done"
+            order = 50,
+          },
+          cancelled = {
+            marker = "󰩹",
+            markdown = "/",
+            type = "complete",
+            order = 2,
+          },
+          pending = {
+            marker = "󰥔",
+            markdown = "-",
+            type = "inactive",
+            order = 100,
+          },
+          reconsider = {
+            marker = "",
+            markdown = "?",
+            type = "inactive",
+            order = 100,
+          },
+          urgent = {
+            marker = "",
+            markdown = "!",
+            type = "inactive",
+            order = 100,
+          },
         },
-        cancelled = {
-          marker = "󰩹",
-          markdown = "/",
-          type = "complete",
-          order = 2,
-        },
-        pending = {
-          marker = "󰥔",
-          markdown = "-",
-          type = "inactive",
-          order = 100,
-        },
-        reconsider = {
-          marker = "",
-          markdown = "?",
-          type = "inactive",
-          order = 100,
-        },
-        urgent = {
-          marker = "",
-          markdown = "!",
-          type = "inactive",
-          order = 100,
-        },
-      },
-    },
+      }
+      local checkmate = require("checkmate")
+      checkmate.setup(opts)
+      local picker = require("checkmate.picker")
+      vim.api.nvim_create_user_command("CheckmateTodos", function()
+        local todos = checkmate.get_todos({
+          filter = { state_types = { "incomplete" } },
+        })
+        picker.pick(picker.map_items(todos, "text"), {
+          method = "pick_todo",
+          {},
+        })
+      end, {})
+    end,
   },
 }
